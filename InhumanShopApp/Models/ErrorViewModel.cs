df28ec5347886485fb61ea7b47952333f7e8e1fc
Belgin <belginmehmed9@gmail.com>
2024-12-03 11:39:50 +0200

    Add project files.

---
+++ InhumanShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InhumanShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
