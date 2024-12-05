254062eb46791305b02e89217b0599e71c99925f
Belgin <belginmehmed9@gmail.com>
2024-12-05 23:28:57 +0200

    Fix structure and change classes and folders. Add a class for constants.

---
+++ InhumanShopApp.Infrastructure/Constants/ValidationConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhumanShopApp.Infrastructure.Constants
+{
+    public class ValidationConstants
+    {
+        public const string requireEmailMessage = "Email is required.";
+        public const string requirePasswordMessage = "Password is required.";
+        public const string passwordLengthErrorMessage = "The {0} must be at {2} and at max {1} characters long.";
+        public const string requireConfirmPasswordMessage = "Confirm Password is required.";
+        public const string notMatchingPasswordsErrorMessage = "Password does not match.";
+        public const string requireNameMessage = "Name is required.";
+
+        public const int passwordMaxLength = 40;
+        public const int passwordMinLength = 8;
+    }
+}
--- InhumanShopApp/Models/VerifyEmailViewModel.cs
+++ InhumanShopApp/Models/VerifyEmailViewModel.cs
@@ -4,12 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static InhumanShopApp.Infrastructure.Constants.ValidationConstants;
 
 namespace InhumanShopApp.Infrastructure.Data.Models
 {
     public class VerifyEmailViewModel
     {
-        [Required(ErrorMessage = "Email is required.")]
+        [Required(ErrorMessage = requireEmailMessage)]
         [EmailAddress]
         public string Email { get; set; }
     }
