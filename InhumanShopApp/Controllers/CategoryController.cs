c2219eec7fcd691456cd66e17db52f6e2342e437
Belgin <belginmehmed9@gmail.com>
2024-12-12 21:48:11 +0200

    Updated AccountController. Added more Cart Product Order Category Controllers.

---
+++ InhumanShopApp/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InhumanShopApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //CRUD operations for categories 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InhumanShopApp/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InhumanShopApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        //CRUD operationd for products [admins]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
