7e7c4d135974a1bcbefa1d67a226fd5d1e35ed60
Belgin <belginmehmed9@gmail.com>
2024-12-13 08:42:35 +0200

    Adding folder for Product Models and adding ProductViewModel.

---
+++ InhumanShopApp/Models/Product/ProductViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Humanizer;
+using InhumanShopApp.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static InhumanShopApp.Infrastructure.Constants.ErrorMessages;
+using static InhumanShopApp.Infrastructure.Constants.ProductConstants;
+namespace InhumanShopApp.Models.Product
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Comment("Product Identifier")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = requireFieldMessage)]
+        [StringLength(productNameMaxLength,
+            MinimumLength = productNameMinLength,
+            ErrorMessage = stringLengthErrorMessage)]
+        [Comment("Product Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = requireFieldMessage)]
+        [Comment("Product count")]
+        public int Count { get; set; }
+
+        [Required(ErrorMessage = requireFieldMessage)]
+        [Comment("Product Price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = requireFieldMessage)]
+        [Comment("Product Category")]
+        public Category Category { get; set; } = null!;
+
+        [Required(ErrorMessage = requireFieldMessage)]
+        [StringLength(productDescriptionMaxLength,
+            MinimumLength = productDescriptionMinLength,
+            ErrorMessage = stringLengthErrorMessage)]
+        [Comment("Product description")]
+        public string Description { get; set; } = string.Empty;
+
+        [Comment("Product image url")]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
